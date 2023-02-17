02bbee665c6548e74115f6e114f29d48135724af
Justin Sher <jwsher@gmail.com>
2023-02-16 18:27:33 -0800

    switched to my repo

--- server/install.sh
+++ server/install.sh
@@ -3,7 +3,7 @@
 mkdir pyg
 cd pyg
 git clone https://github.com/henk717/KoboldAI
-git clone https://github.com/TavernAI/TavernAI
+git clone https://github.com/jwsher/TavernAI
 cd TavernAI
 sudo apt-get install -y npm
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
